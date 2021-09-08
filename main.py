a5396b62eb66aaeeb9e7ac702aa273d2348b1021
VietBT <vietbt6@gmail.com>
2021-09-09 04:26:14 +0700

    change main to 5-fold

--- main.py
+++ main.py
@@ -2,20 +2,21 @@ from network.trainer import train
 
 
 if __name__=="__main__":
-    config = "configs/config.norm.json"
     mbert = "configs/config.mbert.json"
     vibert = "configs/config.vibert.json"
     velectra = "configs/config.velectra.json"
-
+    
     for biaffine in [True, False]:
-        train(config, velectra, "nojoint", n_tokens=50, biaffine=biaffine)
-        train(config, velectra, "norm_to_punc", n_tokens=50, biaffine=biaffine)
-        train(config, velectra, "punc_to_norm", n_tokens=50, biaffine=biaffine)
+        for fold_id in range(5):
+            config = f"configs/config.norm.fold_{fold_id}.json"
+            train(config, velectra, "nojoint", n_tokens=50, biaffine=biaffine)
+            train(config, velectra, "norm_to_punc", n_tokens=50, biaffine=biaffine)
+            train(config, velectra, "punc_to_norm", n_tokens=50, biaffine=biaffine)
 
-        train(config, velectra, "nojoint", n_blocks=4, n_tokens=50, biaffine=biaffine)
-        train(config, velectra, "norm_to_punc", n_blocks=4, n_tokens=50, biaffine=biaffine)
-        train(config, velectra, "punc_to_norm", n_blocks=4, n_tokens=50, biaffine=biaffine)
-        
-        train(config, velectra, "nojoint", biaffine=biaffine)
-        train(config, velectra, "norm_to_punc", biaffine=biaffine)
-        train(config, velectra, "punc_to_norm", biaffine=biaffine)
+            train(config, velectra, "nojoint", n_blocks=4, n_tokens=50, biaffine=biaffine)
+            train(config, velectra, "norm_to_punc", n_blocks=4, n_tokens=50, biaffine=biaffine)
+            train(config, velectra, "punc_to_norm", n_blocks=4, n_tokens=50, biaffine=biaffine)
+            
+            train(config, velectra, "nojoint", biaffine=biaffine)
+            train(config, velectra, "norm_to_punc", biaffine=biaffine)
+            train(config, velectra, "punc_to_norm", biaffine=biaffine)
