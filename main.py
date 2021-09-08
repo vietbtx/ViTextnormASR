398739b8dcfbcc82fd616202253aec88c2a1e214
VietBT <vietbt6@gmail.com>
2021-09-08 07:46:55 +0700

    change training order

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 from network.trainer import train
 
+
 if __name__=="__main__":
     config = "configs/config.norm.json"
     mbert = "configs/config.mbert.json"
     vibert = "configs/config.vibert.json"
     velectra = "configs/config.velectra.json"
 
-    train(config, velectra, "nojoint", n_blocks=4, n_tokens=50)
-    train(config, velectra, "norm_to_punc", n_blocks=4, n_tokens=50)
-    train(config, velectra, "punc_to_norm", n_blocks=4, n_tokens=50)
-    
     train(config, velectra, "nojoint", n_tokens=50)
     train(config, velectra, "norm_to_punc", n_tokens=50)
     train(config, velectra, "punc_to_norm", n_tokens=50)
+
+    train(config, velectra, "nojoint", n_blocks=4, n_tokens=50)
+    train(config, velectra, "norm_to_punc", n_blocks=4, n_tokens=50)
+    train(config, velectra, "punc_to_norm", n_blocks=4, n_tokens=50)
 
     train(config, velectra, "nojoint")
     train(config, velectra, "norm_to_punc")
