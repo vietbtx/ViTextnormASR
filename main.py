c1f18914ef12d99096350b1cd02cf98038e05830
VietBT <vietbt6@gmail.com>
2021-09-09 08:40:50 +0700

    change training orders

--- main.py
+++ main.py
@@ -9,13 +9,14 @@ if __name__=="__main__":
     for biaffine in [True, False]:
         for fold_id in range(5):
             config = f"configs/config.norm.fold_{fold_id}.json"
-            train(config, velectra, "nojoint", n_tokens=50, biaffine=biaffine)
-            train(config, velectra, "norm_to_punc", n_tokens=50, biaffine=biaffine)
-            train(config, velectra, "punc_to_norm", n_tokens=50, biaffine=biaffine)
 
             train(config, velectra, "nojoint", n_blocks=8, n_tokens=50, biaffine=biaffine)
             train(config, velectra, "norm_to_punc", n_blocks=8, n_tokens=50, biaffine=biaffine)
             train(config, velectra, "punc_to_norm", n_blocks=8, n_tokens=50, biaffine=biaffine)
+
+            train(config, velectra, "nojoint", n_tokens=50, biaffine=biaffine)
+            train(config, velectra, "norm_to_punc", n_tokens=50, biaffine=biaffine)
+            train(config, velectra, "punc_to_norm", n_tokens=50, biaffine=biaffine)
             
             train(config, velectra, "nojoint", biaffine=biaffine)
             train(config, velectra, "norm_to_punc", biaffine=biaffine)
